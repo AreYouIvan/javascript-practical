68ea7e013ad0fdb405335c59ba00c0e322f87da8
Alberto Garcia <ivangm180@gmail.com>
2021-09-07 23:43:34 -0500

    Making the short functions

--- functionsFigures.js
+++ functionsFigures.js
@@ -4,13 +4,13 @@ const squareSides = (side) => {
   console.log("Los lados de tu cuadrado miden: " + side + "cm");
 };
 
-squareSides(4);
+
 // Calculating square perimeter.
 const squarePerimeter = (side) => {
-  const SIDES_SQUARE = 4; 
+  const SIDES_SQUARE = 4;
   return side * SIDES_SQUARE;
 };
-squarePerimeter(5)
+squarePerimeter(5);
 
 // Calculating square area.
 const squareArea = (side) => {
@@ -19,52 +19,41 @@ const squareArea = (side) => {
 
 // Código del Triangulo.
 
-const measuringTriangle = (side1, side2, base) => {
   const ladoTriangulo = side1;
   const ladoTriangulo2 = side2;
   const baseTriangulo = base;
 
-  console.group("Propiedades del triángulo:");
 
-  console.log(
-    `Los lados del triángulo miden: ${ladoTriangulo} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`
-  );
 
   const alturaTriangulo = 5.5;
-  console.log(`La altura del triángulo es de: ${alturaTriangulo} cm`);
 
   const perimetroTriangulo = ladoTriangulo + ladoTriangulo2 + baseTriangulo;
-  console.log(`El périmetro del triángulo es de: ${perimetroTriangulo}`);
 
   const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-  console.log(`El área del triángulo es de: ${areaTriangulo} cm^2 `);
 
-  console.groupEnd();
 };
 
 measuringTriangle(3, 3, 5);
 
 // Código del Círculo.
 
-const propertiesOfCircle = (radio) => {
-  console.group("Propiedades del círculo");
-  const PI = Math.PI;
-  console.log(`Este es el valor de PI: ${PI}cm `);
-  // radio
-  const radioCirculo = radio;
-  console.log(`El radio del círculo es de: ${radioCirculo}cm `);
-  // diametro
-  const diametroCirculo = radioCirculo * 2;
-  console.log(`El diámetro del círculo es de: ${diametroCirculo}cm `);
-  // circunferencia
-  const circunferencia = Math.floor(diametroCirculo * PI);
-  console.log(`La circunferencia del círculo es de: ${circunferencia}cm `);
+const PI = Math.PI;
 
-  // area
-  const areaCirculo = Math.floor(radioCirculo * radioCirculo * PI);
-  console.log(`El área del círculo es de: ${areaCirculo}cm `);
+// radio
+const radio = null;
 
-  console.groupEnd();
-};
+// diametro
+function diametro(radio) {
+  return radio * 2;
+}
 
-propertiesOfCircle(4);
+// circunferencia
+
+function circunferencia(diametro, pi) {
+  return Math.floor(diametro * pi);
+}
+
+// area
+function areaCirculo(radio, pi) {
+  Math.floor(radio * radio * pi);
+}
