60a8cd635c314951d933d9706f8a17944242fac2
Ivan Garcia <ivangm180@gmail.com>
2021-09-09 07:57:35 -0500

    Making discount challenge

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
 
 ## Workshop #1: Geometric Figures
 
-- First Step: Define the formules
-- Second Step: Implement forms in JavaScript
+- First Step: Define the formulas
+- Second Step: Implement formulas in JavaScript
+- Third Step: Make functions
+- Fourth Step: Integer JS with HTML
+
+## Workshop #2: Percents and discounts
+
+- First Step: Define the formulas
+- Second Step: Implement formulas in JavaScript
 - Third Step: Make functions
 - Fourth Step: Integer JS with HTML
---
+++ descuentos/descuentos.js
@@ -0,0 +1,25 @@
+const productPrice = document.getElementById("price");
+const productDiscount = document.getElementById("discount");
+const container = document.createElement("div");
+const section = document.querySelector("section");
+
+
+const text = document.createElement("p");
+
+section.append(container);
+container.append(text);
+
+function showPrice() {
+  // Element content value
+  const priceValue = productPrice.value;
+  const discountValue = productDiscount.value;
+  text.textContent = `El precio del producto es ${discount(priceValue, discountValue)}`;
+}
+function discount(price, discount) {
+  const FULL_PRICE = 100;
+  const discountPrice = FULL_PRICE - discount;
+  const newPrice = (price * discountPrice) / FULL_PRICE;
+  return newPrice;
+}
+
+// This is an scalable solution but is easier use the innerText like the class.
