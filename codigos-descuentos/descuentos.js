32f9cf1f63c26b9e60f28664f33df3a36956ed76
Ivan Garcia <ivangm180@gmail.com>
2021-09-11 12:30:19 -0500

    solving the coupons problem

--- codigos-descuentos/descuentos.js
+++ codigos-descuentos/descuentos.js
@@ -1,7 +1,7 @@
 // DOM manipulating library.
 
 const productPrice = document.getElementById("price");
-const productDiscount = document.getElementById("discount");
+const discountCode = document.getElementById("code");
 const container = document.createElement("div");
 const section = document.querySelector("section");
 const text = document.createElement("p");
@@ -25,30 +25,45 @@ const coupons = [
   { name: "cryptocositas", discount: 15 },
   { name: "areyouivan", discount: 10 },
   { name: "sacalasfrias", discount: 40 },
+  { name: "mamador", discount: 50 },
 ];
 
-function validatingCoupons(code) {
-  code = productDiscount.value;
-  let realCode = code.toLowerCase();
+const readableCode = (code) => {
+  code = code.value;
+  return code.toLowerCase();
+};
 
-  if (!realCode) {
+function validatingCoupons(code) {
+  let userCode = readableCode(code);
+  try {
+    for (let coupon of coupons) {
+      if (userCode === coupon.name) {
+        const discountToBeApplied = coupon.discount;
+        return applyingDiscount(discountToBeApplied);
+      }
+    }
     throw new Error("Woops, tu código no es válido..");
-  } else {
-    applyingDiscount();
+
+  } catch (err) {
+    text.textContent = err.message;
   }
+
+}
+
+function applyingDiscount(discount) {
+  return showPrice(discount);
 }
 
 // Logic
-function showPrice() {
+function showPrice(discount) {
   // Element content value
   const priceValue = productPrice.value;
-  const discountValue = productDiscount.value;
-  text.textContent = `El precio del producto es ${discount(
+  text.textContent = `El precio del producto es ${priceWithDiscount(
     priceValue,
-    discountValue
+    discount
   )}`;
 }
-function discount(price, discount) {
+function priceWithDiscount(price, discount) {
   const FULL_PRICE = 100;
   const discountPrice = FULL_PRICE - discount;
   const newPrice = (price * discountPrice) / FULL_PRICE;
